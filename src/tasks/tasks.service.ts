8d47e954e6b9d8e835fad0e2ef68a52455de5045
Kingsley Attasiem <jattasiem@epohcj.com>
2021-02-05 13:50:03 +0100

    feat: validation with class-validator

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,15 +11,28 @@ export class TasksService {
     getAllTasks() {
         return this.tasks;
     };
-    
-    getTasksWithFilters(filterDto: GetTaskFilterDto): Task[]{
-        return this.tasks.filter(task => task.status !== filterDto.status);
+
+    getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
+        const { search, status } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+        if (search) {
+            tasks = tasks.filter(task =>
+                task.title.includes(search) || task.description.includes(search)
+            );
+        }
+
+        return tasks
     }
 
-    getTaskById(id: string): Task{
+    getTaskById(id: string): Task {
         const found = this.tasks.find(task => task.id === id);
-        if(!found){
-          throw new NotFoundException(`Task ID ${id} is not found`);
+        if (!found) {
+            throw new NotFoundException(`Task ID ${id} is not found`);
         }
         return found;
     }
@@ -37,13 +50,13 @@ export class TasksService {
         return task;
     }
 
-    updateTask(id: string, status: TaskStatus): Task{
+    updateTask(id: string, status: TaskStatus): Task {
         const task = this.getTaskById(id);
         task.status = status;
         return task;
     }
 
-    deleteTaskById(id: string): void{
+    deleteTaskById(id: string): void {
         const found = this.getTaskById(id);
         this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
