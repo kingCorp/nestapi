a9360a325a9ab5f27b04a3712879693690b89c86
Kingsley Attasiem <jattasiem@epohcj.com>
2021-02-07 20:49:24 +0100

    feat: using postgres database

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,5 +1,6 @@
 import { IsIn, IsNotEmpty, IsOptional } from "class-validator";
-import { TaskStatus } from "../task.model";
+import { TaskStatus } from "../types/task-status.enum";
+
 
 export class GetTaskFilterDto {
     @IsOptional()
